c8cb9188351c3cf0c2c8eb925ccdbad25191dd51
roberto lupo <46282784+robertolupo@users.noreply.github.com>
2025-06-03 02:13:24 +0200

    version 0.3.1.2 : - adjust lines READ.md, course-info.yaml - reimpostati 0.3.1.1 e 0.3.1.2 su 2 livelli anzicchè su 3 - verificato l'utilizzo di IPython console per 0.3.1.1 - check non funzionante, impostata working directory per import Merge remote-tracking branch 'origin/v0312' into v0312 - check 0.3.1.2 rimessi a posto i placeholders

--- 3.Introduzione informale/3.1 Python come calcolatrice/3.1.2 Stringhe Apici e loro escaping/task.py
+++ 3.Introduzione informale/3.1 Python come calcolatrice/3.1.2 Stringhe Apici e loro escaping/task.py
@@ -9,41 +9,40 @@ def outputConsole():
     `Run File in Python Console' nel menù contestuale dell'editor clicca il bottone destro del mouse
 
 # SVOLGIMENTO: inserire l'output della console a ognuno dei seguenti prompt
-# roberto lupo
+# DA COMPLETARE: inserire il nome e cognome
 
 rl1.1>>>'Questa "parola" è tra doppi apici, quest\'altra \'parola\' invece è tra apici singoli'
-rl1.1'Questa "parola" è tra doppi apici, quest\'altra \'parola\' invece è tra apici singoli'
+rl1.1DA COMPLETARE lasciando il tag rl1.1 a inizio riga
 
 rl1.2>>>"Questa 'parola' è tra apici, quest'altra \"parola\" invece è tra apici doppi"
-rl1.2'Questa \'parola\' è tra apici, quest\'altra "parola" invece è tra apici doppi'
+rl1.2DA COMPLETARE lasciando il tag rl1.2 a inizio riga
 
 rl1.3>>>" Mi 'piaace' così e \"anche\" così"
-rl1.3' Mi \'piaace\' così e "anche" così'
+rl1.3DA COMPLETARE lasciando il tag rl1.3 a inizio riga
 
 rl1.4>>>''' questa 'prevede' anche gli "apici" \'''tripli non usati\''' però nella docstring'''
-rl1.4' questa \'prevede\' anche gli "apici" \'\'\'tripli non usati\'\'\' però nella docstring'
+rl1.4DA COMPLETARE lasciando il tag rl1.4 a inizio riga
 
-rl2.1>>>a = 'stringa che precede il newline\nstringa che succede al newline'
-print(a)
-rl2.1stringa che precede il newlinestringa che succede al newline
+rl2.1>>>a = 'stringa che precede il newline\nstringa che succede al newline'; print(a)
+rl2.1DA COMPLETARE lasciando il tag rl2.1 a inizio riga
 
 rl2.2>>>a
-rl2.2'stringa che precede il newline\qstringa che succede al newline'
+rl2.2DA COMPLETARE lasciando il tag rl2.2 a inizio riga
 
 rl2.3>>>print('C:\\Users\name') # inserire nell'output solo l'ultima riga della risposta
-rl2.3SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \\UXXXXXXXX escape
+rl2.3DA COMPLETARE lasciando il tag rl2.3 a inizio riga
 
 rl2.4>>>print(r'C:\AppData\Roaming')
-rl2.4C:\AppData\Roaming
+rl2.4DA COMPLETARE lasciando il tag rl2.4 a inizio riga
 
 rl2.5>>>print('C:\\\\Users\name')
-rl2.5C:\\Users
-ame
+rl2.5DA COMPLETARE lasciando il tag rl2.5 a inizio riga
 
 rl2.6>>>print('C:\\\\Users\\name')
-rl2.6C:\\Users\name
+rl2.6DA COMPLETARE lasciando il tag rl2.6 a inizio riga
 
 rl2.7>>># SVOLGIMENTO: stringhe su più righe 'as is written' con apici tripli '''
+rl2.7DA COMPLETARE lasciando il tag rl2.7 a inizio riga
     """
     pass
 
